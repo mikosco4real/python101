555f5aedf0b3b650fcc69fbd24d7ee066420e09c
Michael Okolo <okolomikel2009@gmail.com>
2022-07-20 22:49:11 +1000

    Added Lesson 3
    - Discussed recursion in programming
    - Assessment on Fibonacci series

---
+++ lesson03/recursion.py
@@ -0,0 +1,28 @@
+# Recursion...calling a function within itself
+
+def sayhi():
+    print("Hi Michael")
+    sayhi()
+
+
+# Write a function that accepts a number and returns the factorials of that number
+# factorial of 5 is 5*4*3*2*1
+# in general n * (n - 1) * (n - 2) * ... * 1
+# n * (n - 1)!
+
+# Solving the problem using traditional method
+def factorial_traditional(num: int) -> int:
+    result = 1
+    while num > 1:
+        result *= num # result = result * num
+        num -= 1 # num = num - 1
+    
+    return result
+
+def factorial_recursion(num: int) -> int:
+    if num < 2:
+        return 1
+    else:
+        return num * factorial_recursion(num - 1)
+
+print(factorial_recursion(1000))
